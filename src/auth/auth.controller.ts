551213f7f6f681f762f787ddeb49636a325692f5
Muhammad Fajri <fajrijijin@gmail.com>
2024-12-23 10:54:11 +0700

    try fix deploy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -34,7 +34,7 @@ export class AuthController {
     res.cookie('access_token', access_token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       path: '/',
     });
 
