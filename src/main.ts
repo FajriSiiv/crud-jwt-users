870d1ee70d80d10a260a04944faa10d8ac3617d1
Muhammad Fajri <fajrijijin@gmail.com>
2024-12-23 11:51:40 +0700

    try again

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 import { MongooseExceptionFilter } from 'filters/exception.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
